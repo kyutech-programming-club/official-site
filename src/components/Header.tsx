a1925656f141ff0783c93f4dd135fc34689b54f5
oshanQQ <onoyama1999910@gmail.com>
2021-04-09 09:13:06 +0900

    Add gradient border to the top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 const Header = () => {
   return (
+      <div className="sticky top-0 bg-gradient-to-r from-green-400 to-blue-500 h-3"></div>
     <header>
       <div>ahiahi on Header</div>
     </header>
