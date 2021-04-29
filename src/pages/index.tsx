fafd90abb7eb122b30528ba9e681777d0cf9dc03
kei1115 <keifukuda1115@gmail.com>
2021-04-30 00:51:50 +0900

    create

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import 'tailwindcss/tailwind.css';
 import Header from '../components/Header';
+import Link from 'next/link';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Link href="/accordion">
+        <p>accordion</p>
+      </Link>
+      <Link href="/#answer">
+        <a>Q1</a>
+      </Link>
       <p>ahiahi page</p>
       <p>ahiahi page</p>
       <p>ahiahi page</p>
@@ -27,6 +34,7 @@ const Home = () => {
       <p>ahiahi page</p>
       <p>ahiahi page</p>
       <p>ahiahi page</p>
+      <p id="answer">answer</p>
       <p>ahiahi page</p>
       <p>ahiahi page</p>
       <p>ahiahi page</p>
