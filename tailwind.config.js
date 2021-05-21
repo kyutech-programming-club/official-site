398385f2b2f37bb97cb97b3fd7946020500dbdde
runrun000 <mtrunrun@gmail.com>
2021-05-21 19:49:13 +0900

    make round compornent

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,37 @@ module.exports = {
   },
   plugins: [],
 };
+
+const plugin = require('tailwindcss/plugin');
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    plugin(function ({ addComponents }) {
+      const rounded = {
+        '.rounded-l-full': {
+          padding: '.5rem 6rem 5rem 7rem',
+          margin: '.10rem 0 10rem 50rem',
+          borderRadius: '.25rem',
+          fontWeight: '300',
+          backgroundColor: '#DBEAFE',
+          color: '#000',
+          Animation: 'slidein-r',
+        },
+
+        '.rounded-r-full': {
+          padding: '.5rem 7rem 5rem 6rem',
+          margin: '.10rem 50rem 10rem 0',
+          borderRadius: '.25rem',
+          fontWeight: '300',
+          backgroundColor: '#BFDBFE',
+          color: '#000',
+        },
+      };
+
+      addComponents(rounded);
+    }),
+  ],
+};
