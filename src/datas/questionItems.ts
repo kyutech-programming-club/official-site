8dcaf9000829e809d35628a007cddc12170913ac
kei1115 <keifukuda1115@gmail.com>
2021-06-26 10:52:45 +0900

    add sentences

---
+++ src/datas/questionItems.ts
@@ -0,0 +1,27 @@
+import { QuestionType } from '../types/questionsType';
+export const questions: QuestionType[] = [
+  {
+    id: '/#question1',
+    title: 'Q1.どのくらいの頻度で活動すればいいの？',
+  },
+  {
+    id: '/#question2',
+    title: 'Q2.お金はかかるの？',
+  },
+  {
+    id: '/#question3',
+    title: 'Q3.九工大じゃなくても大丈夫？',
+  },
+  {
+    id: '/#question4',
+    title: 'Q4.全くプログラミングについて知らないけど大丈夫？',
+  },
+  {
+    id: '/#question5',
+    title: 'Q5.兼部、兼サークルはできるの？',
+  },
+  {
+    id: '/#question6',
+    title: 'Q6.どれぐらいでプログラミングできるようになる？',
+  },
+];
