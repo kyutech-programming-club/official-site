71e4a1df5841c39117df656f6c2b40b53bfae658
oshanQQ <oshanqq@gmail.com>
2022-02-12 22:09:57 +0900

    🎉 Begin a project!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./components/**/*.tsx', './pages/**/*.tsx'],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-};
+}
