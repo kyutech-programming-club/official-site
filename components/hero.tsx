6e37b4ba81f2205f5f40461cfed3661673fa2774
oshanQQ <oshanqq@gmail.com>
2022-02-17 00:48:00 +0900

    🎨 Hero section can change its image

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,21 +1,31 @@
-const Hero = () => {
+import Link from "next/link";
+
+type Props = {
+  imageUrl: string;
+};
+
+const Hero = ({ imageUrl }: Props) => {
   return (
-    <div
-      className="w-full bg-center bg-cover h-[18rem]"
-      style={{
-        backgroundImage: "url(/assets/hero/hero2.jpg)",
-      }}
-    >
-      <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
-        <div className="text-center">
-          <h1 className="text-5xl font-semibold text-white lg:text-6xl font-mono">
-            Welcome to proken <span className="text-cyan-300">2</span>
-            <span className="text-pink-400">1</span>
-            <span className="text-teal-300">6</span>!
-          </h1>
+    <>
+      <Link href="/">
+        <div
+          className="w-full bg-center bg-cover h-[18rem] cursor-pointer"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        >
+          <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
+            <div className="text-center">
+              <h1 className="text-5xl font-semibold text-white lg:text-6xl font-mono">
+                Welcome to proken <span className="text-cyan-300">2</span>
+                <span className="text-pink-400">1</span>
+                <span className="text-teal-300">6</span>!
+              </h1>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </Link>
+    </>
   );
 };
 
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,6 +1,5 @@
 import Container from "../components/container";
 import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
 import Layout from "../components/layout";
 import { getAllPosts } from "../lib/api";
 import Head from "next/head";
@@ -9,40 +8,21 @@ import Post from "../types/post";
 import Hero from "../components/hero";
 import Section from "../components/section";
 import CustomImage from "../components/custom-image";
-import Activity from "../components/activity";
 
 type Props = {
   allPosts: Post[];
 };
 
 const Index = ({ allPosts }: Props) => {
-  const topPosts = allPosts.slice(0, 3);
+  const topPosts = allPosts;
   return (
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Blog | proken216</title>
         </Head>
 
-        <Hero imageUrl="/assets/hero/hero.jpg" />
-
-        {/* About sction */}
-        <Section
-          index="About"
-          borderColor="cyan"
-          content={CONTENT_IN_ABOUT_SECTION}
-        >
-          <Container>
-            <CustomImage imageUrl="/assets/hero/hero.jpg" alt="hero_image" />
-          </Container>
-        </Section>
-
-        {/* Activity section */}
-        {/* <Section index="Activity" borderColor="pink">
-          <Container>
-            <Activity />
-          </Container>
-        </Section> */}
+        <Hero imageUrl="/assets/hero/hero2.jpg" />
 
         {/* Blog section */}
         <Section index="Blog" borderColor="teal">
